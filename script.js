178f69d65040002f8eb811d1fc85d7aefd11d1bd
Guilherme <guimarquesbrito@gmail.com>
2025-12-28 23:07:56 -0300

    Primeiro commit correto

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-// Botão de Parceria (agora abre o E-mail)
-document
-  .getElementById("parceriaButton")
-  .addEventListener("click", function () {
-    var destinatario = "Sophiaamenezes10@gmail.com"; // Seu e-mail
-    var assunto = "Proposta de Parceria - Sophia Menezes"; // Assunto sugestivo
-
-    // Cria o link para abrir a composição de e-mail do Gmail na Web
-    var gmailLink =
-      "https://mail.google.com/mail/?view=cm&fs=1&to=" +
-      destinatario +
-      "&su=" +
-      encodeURIComponent(assunto);
-
-    // window.open é melhor aqui pois abre em uma nova aba/janela,
-    // mantendo seu site aberto para o usuário.
-    window.open(gmailLink, "_blank");
-  });
-
+document.getElementById("whatsappButton");
+document.getElementById("emailButton").addEventListener("click", function () {
+  var destinatario = "Sophiaamenezes10@gmail.com"; // Substitua pelo seu e-mail
+  var mailtoLink = "mailto:" + destinatario;
+  window.location.href = mailtoLink; // Abre o cliente de e-mail padrão
+});
 // Mídia Kit (abre outro arquivo HTML)
 document
   .getElementById("mediaKitButton")
