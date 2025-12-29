292d30d4051c34cffbc1d28c2a942049d711c831
Guilherme <guimarquesbrito@gmail.com>
2025-12-28 22:54:26 -0300

    Primeiro commit correto

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ document
   .addEventListener("click", function () {
     var destinatario = "Sophiaamenezes10@gmail.com"; // Seu e-mail
     var assunto = "Proposta de Parceria - Sophia Menezes"; // Assunto sugestivo
-    var mailtoLink =
-      "mailto:" + destinatario + "?subject=" + encodeURIComponent(assunto);
 
-    window.location.href = mailtoLink; // Abre o cliente de e-mail padrão
+    // Cria o link para abrir a composição de e-mail do Gmail na Web
+    var gmailLink =
+      "https://mail.google.com/mail/?view=cm&fs=1&to=" +
+      destinatario +
+      "&su=" +
+      encodeURIComponent(assunto);
+
+    // window.open é melhor aqui pois abre em uma nova aba/janela,
+    // mantendo seu site aberto para o usuário.
+    window.open(gmailLink, "_blank");
   });
 
 // Mídia Kit (abre outro arquivo HTML)
